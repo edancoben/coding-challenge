2c9ad73ca52c0844adb5881ebdce03aa06e722c4
edancoben <edancoben8@gmail.com>
2022-08-08 17:42:22 -0500

    adding api app and starting project

--- app/farmer_home/asgi.py
+++ app/farmer_home/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for farmer_admin project.
+ASGI config for farmer_home project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'farmer_admin.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'farmer_home.settings')
 
 application = get_asgi_application()
--- app/farmer_home/urls.py
+++ app/farmer_home/urls.py
@@ -1,4 +1,4 @@
-"""farmer_admin URL Configuration
+"""farmer_home URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
