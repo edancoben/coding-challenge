32451084bf5a91f29b30d60603302af752d19ce2
edancoben <edancoben8@gmail.com>
2022-08-11 12:43:06 -0500

    first view and adding serializers

--- answers/api/views.py
+++ answers/api/views.py
@@ -4,7 +4,7 @@ from .models import WeatherData, WeatherAnalysis, YieldData
 from rest_framework.response import Response
 
 # TODO data should be paginated
-class WeatherViewSet(viewsets.ModelViewSet):
+class WeatherDataViewSet(viewsets.ModelViewSet):
     http_method_names = ["get"]
     serializer_class = WeatherData
 
@@ -20,3 +20,42 @@ class WeatherViewSet(viewsets.ModelViewSet):
             kwargs["weather_station"] = weather_station
 
         queryset = WeatherData.objects.filter(**kwargs)
+
+        return Response(queryset)
+
+
+class YieldDataViewSet(viewsets.ModelViewSet):
+    http_method_names = ["get"]
+    serializer_class = YieldData
+
+    def get_queryset(self):
+        query_params = self.request.query_params
+        year = query_params.get("year")
+
+        kwargs = {}
+        if year:
+            kwargs["year"] = year
+
+        queryset = YieldData.objects.filter(**kwargs)
+
+        return Response(queryset)
+
+
+class WeatherAnalysisViewSet(viewsets.ModelViewSet):
+    http_method_names = ["get"]
+    serializer_class = WeatherAnalysis
+
+    def get_queryset(self):
+        query_params = self.request.query_params
+        year = query_params.get("year")
+        weather_station = query_params.get("weather_station")
+
+        kwargs = {}
+        if year:
+            kwargs["year"] = year
+        if weather_station:
+            kwargs["weather_station"] = weather_station
+
+        queryset = WeatherAnalysis.objects.filter(**kwargs)
+
+        return Response(queryset)
