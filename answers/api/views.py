89deaddcd6fd6a7c23d66c959a65c88af7ec8b52
edancoben <edancoben8@gmail.com>
2022-08-11 12:16:29 -0500

    first view and adding serializers

--- answers/api/views.py
+++ answers/api/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
-from rest_framework import permissions, status, viewsets
+from rest_framework import viewsets
+from .models import WeatherData, WeatherAnalysis, YieldData
+from rest_framework.response import Response
 
-# Create your views here.
+# TODO data should be paginated
+class WeatherViewSet(viewsets.ModelViewSet):
+    http_method_names = ["get"]
+    serializer_class = WeatherData
+
+    def get_queryset(self):
+        query_params = self.request.query_params
+        date = query_params.get("date")
+        weather_station = query_params.get("weather_station")
+
+        kwargs = {}
+        if date:
+            kwargs["date"] = date
+        if weather_station:
+            kwargs["weather_station"] = weather_station
+
+        queryset = WeatherData.objects.filter(**kwargs)
